90e2d96edf19b56109b1137b70e44c7967367e21
Mensurui <mensurk3000@gmail.com>
2023-09-24 14:03:44 +0300

    new input

---
+++ control_flows_and_loops.py
@@ -0,0 +1,7 @@
+favorite_people = ["John", "Mary","Adam", "Alice"]
+
+for person in favorite_people:
+    if person == "Mary":
+        pass
+    print("My favorite person is " + person)
+else: print("My favorite person is not found in this list of favorite people")
