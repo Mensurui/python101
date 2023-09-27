870dea67ee6a196fa5f53b665c0c12491a996652
Mensurui <mensurk3000@gmail.com>
2023-09-27 19:48:16 +0300

    Tuche

--- Chapter2/openingFiles.py
+++ Chapter2/openingFiles.py
@@ -1,7 +1,14 @@
-file = open('text.txt', mode='r')
+
+file = open('test.txt', mode='r')
 
 data = file.read()
 
 print(data)
 
 file.close()
+
+#OR even better
+
+with open('test.txt', mode='r') as test:
+    datum = test.readline()
+print(datum)
