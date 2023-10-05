04b068ee694a7e6aed98c849a4d9b61f7f492816
Mensurui <mensurk3000@gmail.com>
2023-10-05 09:58:14 +0300

    encapsulation

---
+++ Chapter4/encapsulation.py
@@ -0,0 +1,21 @@
+#The idea of encapsulation is to have methods and variables
+#within the bounds of a single unit in the case of python that unit is known as class
+
+class Alpha:
+    def __init__(self):
+        self._a = 2. #Protected member 'a' '_'
+        self.__b = 2. #Private member 'b' '__'
+    def get_b(self):
+        return self.__b
+
+    #def set_b(self, value):
+        #self.__b = value
+
+obj = Alpha()
+valueA = obj._a
+
+valueB = obj.get_b()
+
+print(valueA)
+print(valueB)
+
