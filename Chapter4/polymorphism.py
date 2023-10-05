8be30bf71de7961c54e3b20407e118209cd6cd5e
Mensurui <mensurk3000@gmail.com>
2023-10-05 10:05:02 +0300

    polymorphism

---
+++ Chapter4/polymorphism.py
@@ -0,0 +1,11 @@
+#When an object exhibits many properties
+#by definition everything in inherently an object in python
+
+string = "poly"
+num = 7
+sequence = [1,2,3]
+new_str = string * 3
+new_num = 7 * 3
+new_sequence = sequence * 3
+
+print(new_str, new_num, new_sequence)
