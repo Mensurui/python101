8279b011ad8acf2cc7b2b7418e42f2c7a80501b3
Mensurui <mensurk3000@gmail.com>
2023-10-05 10:34:50 +0300

    classes

---
+++ Chapter4/python_classes_instances_objects.py
@@ -0,0 +1,14 @@
+class MyClass:
+    _hitler = "و قال هتلر"
+    print("Hail hitler")
+    
+    def hello(self):
+        print("The third riech")
+        pass
+    
+myclass = MyClass()
+value = myclass._hitler
+
+print(value)
+
+myclass.hello()
