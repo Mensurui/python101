d0a007c849308a1d33118ff9c13ca22c6ca71cd3
Mensurui <mensurk3000@gmail.com>
2023-10-05 13:55:50 +0300

    Hey there

--- Chapter4/instantiate_a_custom_object.py
+++ Chapter4/instantiate_a_custom_object.py
@@ -2,6 +2,7 @@ class MyFirstClass:
     print ("Who wrote this?")
     index = "Author-Book"
     def hand_list(self, philosopher,book ):
+        print(MyFirstClass.index)
         self.philosopher = philosopher
         self.book = book
         print(self.philosopher + " wrote the book: " + self.book)
@@ -11,4 +12,20 @@ whodunnit = MyFirstClass()
 authors_name = input("Write the name of the author: ")
 name_of_the_book = input("Write the name of the book: ")
 whodunnit.hand_list(authors_name, name_of_the_book)
+
+'''
+OR
+'''
+
+# Sample Solution code
+class MyFirstClass():
+    print("Who wrote this?")
+    index = "Author-Book"
+
+    def hand_list(self, philosopher, book):
+        print(MyFirstClass.index)
+        print(philosopher + " wrote the book: " + book)
+
+whodunnit = MyFirstClass()
+whodunnit.hand_list("Sun Tzu", "The Art of War")
 
