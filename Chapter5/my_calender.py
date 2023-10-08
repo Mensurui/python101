bf43ff4f7d686dec415213982e9d6e8e5ef4465e
Mensurui <mensurk3000@gmail.com>
2023-10-08 11:19:12 +0300

    update

--- Chapter5/my_calender.py
+++ Chapter5/my_calender.py
@@ -4,3 +4,12 @@ location = sys.path
 print(location)
 
 for i in location: print(i)
+
+import calendar
+
+leapdays  = calendar.leapdays(2000, 2050)
+print(leapdays)
+is_it_leap = calendar.isleap(2043)
+print(is_it_leap)
+what_is_the_date_today = calendar.day_name  ==  'today' and calendar.   leapdays == leapdays        
+print(what_is_the_date_today)
