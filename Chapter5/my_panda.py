6868d06ede1c80f1b63e54e7a9ee2cb5863968df
Mensurui <mensurk3000@gmail.com>
2023-10-11 11:55:58 +0300

    here goes nothing pandas

---
+++ Chapter5/my_panda.py
@@ -0,0 +1,19 @@
+#used for data science
+ 
+import pandas as pd
+
+a = pd.DataFrame({'Animals': ['Dog','Cat','Lion','Cow','Elephant'],
+                    'Sounds':['Barks','Meow','Roars','Moo','Trumpet']})
+
+print (a)
+print("================================")
+print (a.describe())
+
+b = pd.DataFrame({
+    "Letters" : ['a', 'b', 'c', 'd', 'e', 'f'],
+    "Numbers" : [12, 7, 9, 3, 5, 1]  })
+
+print(b.sort_values(by="Numbers"))
+print("================================")
+b = b.assign(new_values = b["Numbers"] *3)
+print(b.sort_values(by="Numbers" ))
