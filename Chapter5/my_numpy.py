23db43471b349a76ab05548a3421a2d06549ff6e
Mensurui <mensurk3000@gmail.com>
2023-10-11 11:40:31 +0300

    here goes

---
+++ Chapter5/my_numpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.zeros(10)
+print(a)
+
+b= np.full((2,10), 3)
+print(b)
+
+#I didn't fully understand the application nor the use of linspace
+c = np.linspace(1,2,7)
+print(c)    
+print(type(c))
