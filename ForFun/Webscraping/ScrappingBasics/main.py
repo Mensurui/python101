88204107db4dfa797a4e0c72bed55e95b75402f5
Mensurui <mensurk3000@gmail.com>
2023-10-18 15:13:12 +0300

    commit

--- ForFun/Webscraping/ScrappingBasics/main.py
+++ ForFun/Webscraping/ScrappingBasics/main.py
@@ -2,5 +2,9 @@ from bs4 import BeautifulSoup
 
 with open('home.html', 'r') as html_file:
     content = html_file.read()
+    
     soup = BeautifulSoup(content, 'lxml')
-    print(soup.prettify())
+    
+    tags = soup.find_all('h5')
+    
+    print(tags)
