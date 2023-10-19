23b2bf7d0d034a921707433ffa85f6ec7eefecc1
Mensurui <mensurk3000@gmail.com>
2023-10-19 10:32:06 +0300

    django

--- ForFun/Webscraping/Requests and Data Filtrations/main.py
+++ ForFun/Webscraping/Requests and Data Filtrations/main.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
+print("Put some skills you are not familiar or interested in")
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
+print(f'Loading...')
+
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 
 soup = BeautifulSoup(html_text, 'lxml')
@@ -11,14 +16,15 @@ for job in jobs:
     if 'few' in date:
         company_name = job.find('h3', class_='joblist-comp-name').text.strip()
         skills = job.find('span', class_='srp-skills').text.strip()
-        count += 1
+        more_info = job.header.h2.a['href'].strip()
+        if unfamiliar_skill not in skills:
+            count += 1
 
-        print(f'''
-        Company Name: {company_name}
-        Required Skills: {skills}
-        ''')
+            print(f'Company Name: {company_name}')
+            print(f'Required Skills: {skills}')
+            print(f'More Information: {more_info}')
     
-        print('________________________________________________________________')
+            print('_____________________________________________________________________________________________________________________________')
         
 
 print(f'Found {count} jobs that match your criteria')
