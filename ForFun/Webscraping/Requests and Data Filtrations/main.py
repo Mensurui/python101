59d0e3405b85ac5ccd1bf60039931bb398e362fa
Mensurui <mensurk3000@gmail.com>
2023-10-19 10:20:05 +0300

    h

--- ForFun/Webscraping/Requests and Data Filtrations/main.py
+++ ForFun/Webscraping/Requests and Data Filtrations/main.py
@@ -5,15 +5,20 @@ html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?se
 
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+count = 0
 for job in jobs:
-    company_name = job.find('h3', class_='joblist-comp-name').text.strip()
-    skills = job.find('span', class_='srp-skills').text.strip()
     date = job.find('span', class_='sim-posted').span.text.strip()
+    if 'few' in date:
+        company_name = job.find('h3', class_='joblist-comp-name').text.strip()
+        skills = job.find('span', class_='srp-skills').text.strip()
+        count += 1
 
-
-    print(f'''
-    Company Name: {company_name}
-    Required Skills: {skills}
-      ''')
+        print(f'''
+        Company Name: {company_name}
+        Required Skills: {skills}
+        ''')
     
-    print('________________________________________________________________')
+        print('________________________________________________________________')
+        
+
+print(f'Found {count} jobs that match your criteria')
