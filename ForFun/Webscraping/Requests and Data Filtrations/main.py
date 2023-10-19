d8c4cc5122ef75897cf7433ca9b2a1e18da7768d
Mensurui <mensurk3000@gmail.com>
2023-10-19 09:59:38 +0300

    h

--- ForFun/Webscraping/Requests and Data Filtrations/main.py
+++ ForFun/Webscraping/Requests and Data Filtrations/main.py
@@ -4,5 +4,5 @@ import requests
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 
 soup = BeautifulSoup(html_text, 'lxml')
-work_cards = soup.findAll('ul')
+work_cards = soup.find('li', class_='clearfix job-bx wht-shd-bx')
 print(work_cards)
