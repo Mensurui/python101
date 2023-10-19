019ece2e6e7898cec2a6f21132f8fc6eb6ada14c
Mensurui <mensurk3000@gmail.com>
2023-10-19 10:15:30 +0300

    h

--- ForFun/Webscraping/Requests and Data Filtrations/main.py
+++ ForFun/Webscraping/Requests and Data Filtrations/main.py
@@ -4,6 +4,16 @@ import requests
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 
 soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_='joblist-comp-name').text.strip()
-print(company_name)
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+for job in jobs:
+    company_name = job.find('h3', class_='joblist-comp-name').text.strip()
+    skills = job.find('span', class_='srp-skills').text.strip()
+    date = job.find('span', class_='sim-posted').span.text.strip()
+
+
+    print(f'''
+    Company Name: {company_name}
+    Required Skills: {skills}
+      ''')
+    
+    print('________________________________________________________________')
