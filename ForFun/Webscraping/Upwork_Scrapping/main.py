44941d92174bd7053567fcbb96071d8a20a61e92
Mensurui <mensurk3000@gmail.com>
2023-10-21 10:58:46 +0300

    scrappe one

--- ForFun/Webscraping/Upwork_Scrapping/main.py
+++ ForFun/Webscraping/Upwork_Scrapping/main.py
@@ -2,9 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 # Make a request to the website
-res = requests.get("https://finance.yahoo.com/u/yahoo-finance/watchlists/crypto-top-tokens-outstanding")
+res = requests.get("https://www.mekina.net/cars/?body_type=Sedan")
 
 # Parse the page with BeautifulSoup
 soup = BeautifulSoup(res.text, 'lxml')
 # Print the page title
-print(soup.title.string)
+cars = soup.find_all('div', class_='category-grid-box-1')
+
+for car in cars:
+    car_name = car.find('h3').text.strip()
+    print(car_name)
